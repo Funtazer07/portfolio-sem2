ce82ea145222b8d91a4c80b015dfe94f396e5589
Andrejs <533293@student.fontys.nl>
2024-03-26 14:22:56 +0100

    added animation of scrolling

---
+++ myscript.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var scrollFactor = 0.2; // Adjust the scroll factor as needed
+  
+    // Move #dark and #bright elements slightly to the right based on scroll position
+    document.getElementById('dark').style.marginLeft = (6 + scrollPosition * scrollFactor) + 'em';
+    document.getElementById('bright').style.marginLeft = (-scrollPosition * scrollFactor) + 'em';
+  });
+  
